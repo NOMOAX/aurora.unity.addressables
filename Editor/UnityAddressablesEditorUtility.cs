70881c5544c575c9dc7b0ae1a7490f9258e853ee
谢凯文 <844038603@qq.com>
2025-08-02 18:09:54 +0800

    添加 UnityAddressablesEditorUtility 工具类

---
+++ Editor/UnityAddressablesEditorUtility.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Build;
+using UnityEditor.AddressableAssets.Build.DataBuilders;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEngine.AddressableAssets;
+
+namespace Aurora.UnityEditor.Addressables.Editor
+{
+    /// <summary>
+    /// 编辑器工具集。
+    /// </summary>
+    public static class UnityAddressablesEditorUtility
+    {
+        /// <summary>
+        /// 播放模式索引。
+        /// </summary>
+        public static int PlayModeIndex
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => ProjectConfigData.ActivePlayModeIndex;
+        }
+
+        /// <summary>
+        /// 播放模式名称。
+        /// </summary>
+        public static string PlayModeName
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => GetPlayModeName(AddressableAssetSettingsDefaultObject.Settings.GetDataBuilder(PlayModeIndex));
+        }
+
+        private static string ActivePlatformName
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => PlatformMappingService.GetPlatformPathSubFolder();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static string GetPlayModeName(IDataBuilder dataBuilder)
+        {
+            return dataBuilder switch
+            {
+                null                      => "null",
+                BuildScriptPackedPlayMode => $"Use Existing Build ({ActivePlatformName})",
+                _                         => dataBuilder.Name
+            };
+        }
+    }
+}
